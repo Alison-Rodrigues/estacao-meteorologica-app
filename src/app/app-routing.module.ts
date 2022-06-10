26793057dc15b4b14f5d4c1d86b1bcf4e1a8d9fc
Alison-Rodrigues <alison.si1302@gmail.com>
2022-06-10 12:10:19 -0300

    criação do weatherComponent e rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WeatherComponent } from './components/weather/weather.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'index', component: WeatherComponent},
+  { path: '', redirectTo: 'index', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
