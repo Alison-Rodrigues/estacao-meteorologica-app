cc7a32daac5e173cf9d6c7ce9b43c31affb0d531
Alison-Rodrigues <alison.si1302@gmail.com>
2022-07-12 00:50:13 -0300

    update server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + './dist/estacao-meteorologica'));
+app.use(express.static(__dirname + '/dist/estacao-meteorologica'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/estacao-meteorologica/'}),
